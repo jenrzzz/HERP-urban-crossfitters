ffd7d2a81738bf7ea7a75a159139e52b8347504b
Jenner LaFave <jlafave@ucsd.edu>
2012-04-24 15:35:21 -0700

    Built rails scaffolding.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Herpcs110s::Application.config.secret_token = '44efc4959f070653e70b5804052bdc2a87d4b4c35f3d09f14ce366c8c2b08aa0cb55bce3431a8535768a77dbaf1ac90a521a1ca58ea304e9a0a28959ff6f7723'
