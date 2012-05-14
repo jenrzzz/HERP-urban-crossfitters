262213766bf9e038780d3b5d8848897ae7f17dba
Jenner LaFave <jenner@jfave.com>
2012-05-13 18:41:09 -0700

    Added db:sessions:create migration to use ActiveRecord for our session stores.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Herpcs110s::Application.config.session_store :cookie_store, key: '_herpcs110s_session'
+Herpcs110s::Application.config.session_store :active_record_store
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
