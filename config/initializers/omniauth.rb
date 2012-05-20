26ddcea654e3ed4a18d9fde87832e2075eca0aef
Chu Shao <cshao@ucsd.edu>
2012-05-20 00:19:06 -0700

    fixed the scope.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
     #provider :developer unless Rails.env.production?
     provider :facebook, APP_CONFIG['facebook']['app_id'], APP_CONFIG['facebook']['app_secret'],
-        :scope => 'id,name,picture,gender,user_birthday,email,publish_stream',
+        :scope => 'email,user_birthday,email,publish_stream',
         :display => 'touch'
 end
