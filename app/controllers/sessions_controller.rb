e113be4b0f108485d6f34a0fe312ec4bb50cacaa
Patrick Torbett <patrick.torbett@gmail.com>
2012-05-20 11:12:56 -0700

    changed formatting of session_controller (2 space indents, <80 chars) and added an condition for when user.save == false

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,29 +3,39 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	auth_hash = request.env['omniauth.auth']
-  	
-  	#Authentication utilizing provider and UID
-  	@authorization = Authorization.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
-  	if @authorization 
-  	   render :text => "Welcome back #{@authorization.user.name}!"
-  	else
-  		user = User.new :name => auth_hash["info"]["name"], :email => auth_hash["info"]["email"]
-  		user.authorizations.build :provider => auth_hash["provider"], :uid => auth_hash["uid"]
-  		user.save
-  		
-  		render :text => "Congratulations #{user.name}! You have just signed up."
-	end  	
+    auth_hash = request.env['omniauth.auth']
+	
+    #Authentication utilizing provider and UID
+    @authorization =
+      Authorization.find_by_provider_and_uid(auth_hash["provider"],
+                                             auth_hash["uid"])
+    if @authorization
+      #returning user
+      render :text => "Welcome back #{@authorization.user.name}!"
+    else
+      #new user
+      user = User.new(:name => auth_hash["info"]["name"],
+                      :email => auth_hash["info"]["email"])
 
+      user.authorizations.build(:provider => auth_hash["provider"],
+                                :uid => auth_hash["uid"])
+       if user.save
+         #new user created
+         render :text => "Congratulations #{user.name}! You have just signed up."
+       else
+         #print out errors if user didn't save
+         puts user.errors
+         session.failure
+       end
+    end  	
   end
   
   def destroy
-  	session[:user_id] = nil
-  	render :text => "You have successfully logged out!"
+    session[:user_id] = nil
+    render :text => "You have successfully logged out!"
   end
   	
-
   def failure
-  	render :text => "You just messed up somewhere. no clue where but you just did"
+    render :text => "You just messed up somewhere. no clue where but you just did"
   end
 end
