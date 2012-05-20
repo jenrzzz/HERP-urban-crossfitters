9f0ca73fe2fe8d5b30ccb4db5cf0def771624965
Patrick Torbett <patrick.torbett@gmail.com>
2012-05-19 20:54:29 -0700

    reverted a change to facebook provider so dev server should work with this config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
     #provider :developer unless Rails.env.production?
-    provider :facebook, ENV['293232480769280'], ENV['0b4329dfa2552caf5d25e449216fa9cb'],
+    provider :facebook, APP_CONFIG['facebook']['app_id'], APP_CONFIG['facebook']['app_secret'],
         :scope => 'id,name,picture,gender,user_birthday,email,publish_stream',
         :display => 'touch'
 end
