f998ff4ddc48fa3bffda2f7f20b3946aca6a7a5d
Jenner LaFave <jenner@jfave.com>
2012-05-20 15:01:13 -0700

    Generated scaffold for Dashboard controller.

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,17 @@ Herpcs110s::Application.routes.draw do
 
   resources :session
   resource  :profile
-  get "home/index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
   
   #Get Connect with facebook link
   get '/login', :to => 'sessions#new', :as => :login
+  get '/logout', :to => 'sessions#destroy'
 
   #Creates authentication for omni-auth
   match '/auth/:provider/callback' => 'sessions#create'
   match '/auth/failure', :to => 'sessions#failure'
 
-  get '/logout', :to => 'sessions#destroy'
   root :to => "home#index"
 end
