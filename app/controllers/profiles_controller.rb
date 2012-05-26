462e8cd0c358e201562a538e8048051223191962
Jenner LaFave <jenner@jfave.com>
2012-05-26 00:58:55 -0700

    Profiles aren't a singular resource and tweaked profile#show controller method and view.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -29,7 +29,10 @@ class ProfilesController < ApplicationController
   end
 
   def show
-    @profile = Profile.find params[:id] if params[:id] or current_user.profile
+    if params[:id] then
+      @profile = Profile.find params[:id]
+    else 
+      @profile = current_user.profile
     @title = "#{@profile.first_name} #{@profile.last_name}"
   end
 
