d794d84e16e3bb1d23612197b99f960b452a88d3
Patrick Torbett <patrick.torbett@gmail.com>
2012-05-28 11:54:34 -0700

    deleted code in workout controller that does not make sense

--- app/controllers/workout_controller.rb
+++ app/controllers/workout_controller.rb
@@ -9,25 +9,18 @@ class WorkoutController < ApplicationController
 
   # create a new workout
   def create
-    @workout = Workout.create(params[:workout])
-    current_user.workout_records << @workout
-    redirect_to :action => 'show', :id => @workout.id
   end
 
   # display a specific workout
   def show
-    @workout = Workout.find_by_id( params[:id] )
-    render :show
   end
 
   # return a form to edit a workout
   def edit
-    render :edit
   end
 
   # update a specific workout
   def update
-    @workout = Workout.find_by_id( params[:id] )
   end
 
   # delete a specific workout
