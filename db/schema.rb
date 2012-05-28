4ed1bd98644e45a86b0e68625016b02f00987dcf
Jenner LaFave <jenner@jfave.com>
2012-05-27 23:18:00 -0700

    Added migration for (remove name) (add username) in Users.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120526051047) do
+ActiveRecord::Schema.define(:version => 20120528061715) do
 
   create_table "authorizations", :force => true do |t|
     t.string   "provider"
@@ -117,10 +117,10 @@ ActiveRecord::Schema.define(:version => 20120526051047) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "name"
     t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "username"
   end
 
   create_table "workout_records", :force => true do |t|
