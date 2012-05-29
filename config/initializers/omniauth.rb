30a5586c8e0d68cc2d1c51885739d3a7e74a9f81
Jenner LaFave <jenner@jfave.com>
2012-05-28 23:32:35 -0700

    Removed :display parameter from OmniAuth's initializer because it was unnecessary and causing Facebook integration issues.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
     provider :developer unless Rails.env.production?
     provider :facebook, APP_CONFIG['facebook']['app_id'], APP_CONFIG['facebook']['app_secret'],
-        :scope => 'email,user_birthday,email,publish_stream',
-        :display => 'touch'
+        :scope => 'email,user_birthday,email,publish_stream'
 end
