6e0240880b2f171e8a64e55815b0b607e93d0fc6
Jesse Wu <jessewu@JWU.(none)>
2012-06-01 14:14:25 -0700

    seeds example workout

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,9 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.delete_all
-WorkoutCategory.delete_all
-Workout.delete_all
-Exercise.delete_all
+#ISSUE: should be using create! and save! instead of create & save
+
+[User, WorkoutCategory, Workout, Exercise, ExerciseWorkout].each(&:delete_all)
 
 admin = User.create(:email => "admin@test.test", :username => "admin")
 
@@ -17,30 +16,40 @@ girl = WorkoutCategory.create(:category => "Girl")
 hero = WorkoutCategory.create(:category => "Hero")
 wod = WorkoutCategory.create(:category => "WOD")
 benchmark = WorkoutCategory.create(:category => "Benchmark")
-#custom workout category?
 
-[girl, hero, wod, benchmark].each do |category|
+#add to app/models/workout_category.rb - valid category?
+#olympic = WorkoutCategory.create(:category => "Olympic Lifts")
+
+#need to seed custom workout category? all user defined
+#custom = WorkoutCategory.create(:category => "Custom")
+
+[girl, hero, wod, benchmark].each do |category| #add olympic/custom
 	category.user_id = admin.id
 	category.save
 end
 
 angie = Workout.create(:name => "Angie", :description => "For Time
 Complete all reps of each exercise before moving to the next.")
-#angieWorkout = angie.exercises.create
-#([
-#	{:repetitions => 100, :description => "Pull-ups", :user_id => admin.id},
-#	{:repetitions => 100, :description => "Push-ups", :user_id => admin.id},
-#	{:repetitions => 100, :description => "Sit-ups", :user_id => admin.id},
-#	{:repetitions => 100, :description => "Squats", :user_id => admin.id}
-#])
+angie.workout_category_id = girl.id
+angie.user_id = admin.id
+angie.save
 
-[angie].each do |girls|
-	girls.workout_category_id = girl.id
-	girls.user_id = admin.id
-	girls.save
-end
+exercise = angie.exercises.create(:repetitions => 100, :description => "Pull-ups")
+exercise.user_id = admin.id
+exercise.save
+exercise = angie.exercises.create(:repetitions => 100, :description => "Push-ups")
+exercise.user_id = admin.id
+exercise.save
+exercise = angie.exercises.create(:repetitions => 100, :description => "Sit-ups")
+exercise.user_id = admin.id
+exercise.save
+exercise = angie.exercises.create(:repetitions => 100, :description => "Squats")
+exercise.user_id = admin.id
+exercise.save
 
-#Exercise.create(:repetitions => 100, :description => "Pull-ups")
-#Exercise.create(:repetitions => 100, :description => "Push-ups")
-#Exercise.create(:repetitions => 100, :description => "Sit-ups")
-#Exercise.create(:repetitions => 100, :description => "Squats")
+#ISSUE: Does this even need to be created or is it self-generated from above?
+#ERROR: ID's cannot be null, cannot assign protected attributes
+
+#ew = angie.exercise_workouts.create
+#ew.workout_id = angie.id
+#ew.exercise_id = exercise.id
