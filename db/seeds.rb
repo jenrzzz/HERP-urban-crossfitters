7fd62914fa767f15738415f371674fddb52fadeb
Jesse Wu <jessewu@JWU.(none)>
2012-06-01 15:28:40 -0700

    seeds - probably not a good idea to overwrite all exercises

--- db/seeds.rb
+++ db/seeds.rb
@@ -48,22 +48,22 @@ angie.workout_category_id = girl.id
 angie.user_id = admin.id
 angie.save
 
-exercise = angie.exercises.create(:repetitions => 100)
-exercise.exercise_category_id = pullups.id
-exercise.user_id = admin.id
-exercise.save
-exercise = angie.exercises.create(:repetitions => 100)
-exercise.exercise_category_id = pushups.id
-exercise.user_id = admin.id
-exercise.save
-exercise = angie.exercises.create(:repetitions => 100)
-exercise.exercise_category_id = situps.id
-exercise.user_id = admin.id
-exercise.save
-exercise = angie.exercises.create(:repetitions => 100)
-exercise.exercise_category_id = squats.id
-exercise.user_id = admin.id
-exercise.save
+angie1 = angie.exercises.create(:repetitions => 100)
+angie1.exercise_category_id = pullups.id
+angie1.user_id = admin.id
+angie1.save
+angie2 = angie.exercises.create(:repetitions => 100)
+angie2.exercise_category_id = pushups.id
+angie2.user_id = admin.id
+angie2.save
+angie3 = angie.exercises.create(:repetitions => 100)
+angie3.exercise_category_id = situps.id
+angie3.user_id = admin.id
+angie3.save
+angie4 = angie.exercises.create(:repetitions => 100)
+angie4.exercise_category_id = squats.id
+angie4.user_id = admin.id
+angie4.save
 
 #ISSUE: Does this even need to be created or is it self-generated from above?
 #ERROR: ID's cannot be null, cannot assign protected attributes
@@ -75,22 +75,22 @@ barbara = Workout.create(:name => "Barbara", :description => "5 rounds, time eac
 barbara.workout_category_id = girl.id
 barbara.user_id = admin.id
 barbara.save
-exercise = barbara.exercises.create(:repetitions => 20, :rounds => 5)
-exercise.exercise_category_id = pullups.id
-exercise.user_id = admin.id
-exercise.save
-exercise = barbara.exercises.create(:repetitions => 30, :rounds => 5)
-exercise.exercise_category_id = pushups.id
-exercise.user_id = admin.id
-exercise.save
-exercise = barbara.exercises.create(:repetitions => 40, :rounds => 5)
-exercise.exercise_category_id = situps.id
-exercise.user_id = admin.id
-exercise.save
-exercise = barbara.exercises.create(:repetitions => 50, :rounds => 5)
-exercise.exercise_category_id = squats.id
-exercise.user_id = admin.id
-exercise.save
+barbara1 = barbara.exercises.create(:repetitions => 20, :rounds => 5)
+barbara1.exercise_category_id = pullups.id
+barbara1.user_id = admin.id
+barbara1.save
+barbara2 = barbara.exercises.create(:repetitions => 30, :rounds => 5)
+barbara2.exercise_category_id = pushups.id
+barbara2.user_id = admin.id
+barbara2.save
+barbara3 = barbara.exercises.create(:repetitions => 40, :rounds => 5)
+barbara3.exercise_category_id = situps.id
+barbara3.user_id = admin.id
+barbara3.save
+barbara4 = barbara.exercises.create(:repetitions => 50, :rounds => 5)
+barbara4.exercise_category_id = squats.id
+barbara4.user_id = admin.id
+barbara4.save
 #HEROES-----------------------------------------------------------------------
 jt = Workout.create(:name => "JT", :description => "21-15-9 reps, for time.")
 jt.workout_category_id = hero.id
@@ -99,36 +99,36 @@ jt.save
 
 #ISSUE: How to input 21-15-9 reps?
 
-exercise = jt.exercises.create(:repetitions => 21)
-exercise.exercise_category_id = handstand_pushups.id
-exercise.user_id = admin.id
-exercise.save
-exercise = jt.exercises.create(:repetitions => 21)
-exercise.exercise_category_id = ring_dips.id
-exercise.user_id = admin.id
-exercise.save
-exercise = jt.exercises.create(:repetitions => 21)
-exercise.exercise_category_id = pushups.id
-exercise.user_id = admin.id
-exercise.save
+jt1 = jt.exercises.create(:repetitions => 21)
+jt1.exercise_category_id = handstand_pushups.id
+jt1.user_id = admin.id
+jt1.save
+jt2 = jt.exercises.create(:repetitions => 21)
+jt2.exercise_category_id = ring_dips.id
+jt2.user_id = admin.id
+jt2.save
+jt3 = jt.exercises.create(:repetitions => 21)
+jt3.exercise_category_id = pushups.id
+jt3.user_id = admin.id
+jt3.save
 
 
 michael = Workout.create(:name => "Michael", :description => "3 rounds for time.")
 michael.workout_category_id = hero.id
 michael.user_id = admin.id
 michael.save
-exercise = michael.exercises.create(:distance => 800, :units => "meters")
-exercise.exercise_category_id = running.id
-exercise.user_id = admin.id
-exercise.save
-exercise = michael.exercises.create(:repetitions => 50)
-exercise.exercise_category_id = back_extensions.id
-exercise.user_id = admin.id
-exercise.save
-exercise = michael.exercises.create(:repetitions => 50)
-exercise.exercise_category_id = situps.id
-exercise.user_id = admin.id
-exercise.save
+michael1 = michael.exercises.create(:distance => 800, :units => "meters")
+michael1.exercise_category_id = running.id
+michael1.user_id = admin.id
+michael1.save
+michael2 = michael.exercises.create(:repetitions => 50)
+michael2.exercise_category_id = back_extensions.id
+michael2.user_id = admin.id
+michael2.save
+michael3 = michael.exercises.create(:repetitions => 50)
+michael3.exercise_category_id = situps.id
+michael3.user_id = admin.id
+michael3.save
 #WODS-------------------------------------------------------------------------
 #BENCHMARK--------------------------------------------------------------------
 
