2647b42081d43b470c9cecf8d3fa7f7bb63ec4c3
Jenner LaFave <jenner@jfave.com>
2012-06-04 16:01:24 -0700

    Added some user-specific functionality to event_calendar controllers.

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -9,12 +9,4 @@ class CalendarController < ApplicationController
     @event_strips = current_user.events.event_strips_for_month(@shown_month)
   end
   
-  def new
-  end
-  
-  def create
-    current_user.events.new params[:event]
-    redirect_to :action => 'index'
-  end
-  
 end
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,22 +1,31 @@
 class EventsController < ApplicationController
   def index
+    redirect_to 'calendar#index'
   end
 
   def show
+    current_user.events.find params[:id]
   end
 
   def new
-  end
-
-  def edit
+    @event = Event.new
   end
 
   def create
+    current_user.events.create params[:event]
+  end
+  
+  def edit
+    @event = current_user.events.find params[:id]
   end
 
   def update
+    @event = current_user.events.find params[:id]
+    @event.update_attributes params[:event]
   end
 
   def destroy
+    @event = current_user.events.find params[:id]
+    @event.destroy
   end
 end
