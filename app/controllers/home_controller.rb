821943a2941d1b440245195fa21b5dcc737878c3
Jenner LaFave <jenner@jfave.com>
2012-06-06 03:12:01 -0700

    Fixed bad redirect on home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   skip_before_filter :require_login
   def index
-    redirect_to 'dashboards#show' if current_user
+    redirect_to :controller => 'dashboards', :action => 'show' if current_user
     @title = 'Home'
   end
 end
