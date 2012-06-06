bc6b0eeca6673c593af3ad8b6f9db84bf9b2c392
Chu Shao <churen.shao@gmail.com>
2012-06-06 05:11:09 -0700

    Still testing fb connect…

--- app/helpers/workout_records_helper.rb
+++ app/helpers/workout_records_helper.rb
@@ -1,2 +1,37 @@
 module WorkoutRecordsHelper
+
+def fbwallpost
+    if session[:fbaccess]['token'] then
+		@graph =	Koala::Facebook::API.new session[:fbaccess]['token']
+		if @workout_record.time && !@workout_record.points && !@workout_record.rounds
+      $fbwall = "I just completed "+@workout_record.workout.workout_category.category+"-"+@workout_record.workout.name+"in "+@workout_record.time[:string] 
+      end 
+    if !@workout_record.time && !@workout_record.points && @workout_record.rounds  
+        $fbwall = "I just completed "+@workout_record.rounds+ "rounds of "+@workout_record.workout.workout_category.category+"-"+@workout_record.workout.name   
+        end  
+      
+        if !@workout_record.time && @workout_record.points && !@workout_record.rounds  
+        $fbwall = "I just gained "+@workout_record.points+"of points from " +@workout_record.workout.workout_category.category+"-"+@workout_record.workout.name   
+        end  
+      
+        if @workout_record.time && @workout_record.points && !@workout_record.rounds  
+        $fbwall = "I just gained "+@workout_record.points+"of points from "+ @workout_record.workout.workout_category.category+"-"+@workout_record.workout.name+"in "+@workout_record.time[:string]  
+        end  
+      
+        if @workout_record.time && !@workout_record.points && @workout_record.rounds  
+        $fbwall = "I just completed "+@workout_record.rounds+"of points from "+ @workout_record.workout.workout_category.category+"-"+@workout_record.workout.name+"in "+@workout_record.time[:string]  
+        end  
+      
+        if !@workout_record.time && @workout_record.points && @workout_record.rounds  
+        $fbwall = "I just gained "+@workout_record.points+"of points from "+ @workout_record.workout.workout_category.category+"-"+@workout_record.workout.name+"in "+@workout_record.rounds+ " rounds"  
+        end  
+      
+        if @workout_record.time && @workout_record.points && @workout_record.rounds  
+        $fbwall = "I just gained "+@workout_record.points+"of points from "+ @workout_record.workout.workout_category.category+"-"+@workout_record.workout.name+"in "+@workout_record.time[:string]+ " while doing "+ @workout_record.rounds+" rounds"  
+        end  
+  
+		@postfb = @graph.put_wall_post($fbwall)	
+  	end
+  	
+  	end
 end
