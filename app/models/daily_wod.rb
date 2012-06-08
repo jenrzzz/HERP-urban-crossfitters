2f563c52f49192078c62042ee9897b46033eac8f
Patrick Torbett <patrick.torbett@gmail.com>
2012-06-08 16:36:37 -0700

    cleaned up layer violations with daily wods

--- app/models/daily_wod.rb
+++ app/models/daily_wod.rb
@@ -3,4 +3,5 @@
 # and date as scraped from the CrossFit website.
 class DailyWod < ActiveRecord::Base
   attr_accessible :date, :title, :description, :link
+  scope ordered, :order => 'created_at DESC'
 end
