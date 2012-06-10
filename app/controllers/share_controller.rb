073945ce7a410c4025312f93d285280c8490686d
Chu Shao <churen.shao@gmail.com>
2012-06-09 21:35:09 -0700

    Finafuckingly Done with Facebook posting…

--- app/controllers/share_controller.rb
+++ app/controllers/share_controller.rb
@@ -1,29 +1,24 @@
 class ShareController < ApplicationController
 
 
-
-    def create
-        string = params[:fb_string]
-        #raise params.to_yaml
-	    if session[:fbaccess]['token'] then
-	  		@graph = Koala::Facebook::API.new session[:fbaccess]['token']
-  		begin	
-            @graph.put_wall_post($string) 
-
-        rescue 
-            flash[:notice] = 'You have already posted this workout to Facebook!'
-        else
-            flash[:notice] = 'Successfully posted on Facebook!'
-        ensure
-            redirect_to(:back)
+ def create
+#raise params.to_yaml
         
-        end
-  		end
-  	end
-
-    def show
-        redirect_to(:back)
+          if session[:fbaccess]['token'] then
+             @graph = Koala::Facebook::API.new session[:fbaccess]['token']
+            begin   
+							@graph.put_wall_post(params[:fb_string])
+            rescue 
+                flash[:error] = 'You have already posted this workout to Facebook.'
+            else
+                flash[:notice] = 'Successfully posted on Facebook!'
+            ensure
+                redirect_to(:back)
+                return
+            end
+        
     end
-
+  
     
+end
 end
